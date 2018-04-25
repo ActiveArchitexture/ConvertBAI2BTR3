7ac44a73891223bdfddfdfce1bf6668ebd045939
Ross Kendle <ross.kendle@gmail.com>
2018-04-25 23:59:14 +1000

    Start wrapping the cloud function incarnation of the converter.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const converter = require('bai-btrs-active-parsers');
+
 /**
  * Responds to any HTTP request that can provide a "message" field in the body.
  *
@@ -7,11 +9,11 @@
 exports.convert = (req, res) => {
     // Example input: {"message": "Hello!"}
     if (req.body.message === undefined) {
-      // This is an error case, as "message" is required.
-      res.status(400).send('No message defined!');
+        // This is an error case, as "message" is required.
+        res.status(400).send('No message defined!');
     } else {
-      // Everything is okay.
-      console.log(req.body.message);
-      res.status(200).send('Success: ' + req.body.message);
+        // Everything is okay.
+        console.log(req.body.message);
+        res.status(200).send('Success: ' + req.body.message);
     }
-  };
+};
